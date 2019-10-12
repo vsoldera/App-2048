f81a103268a2fd26a86728448d81f3aec3cd1006
Luiz <53343378+luizvruoso@users.noreply.github.com>
2019-10-12 11:25:02 -0300

    Criacao do projeto - Android Studio

---
+++ APP Android/app/src/main/java/com/example/control_for_2048/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.control_for_2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
