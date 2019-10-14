5eaed96a118053ade8be927375b18597a857cb90
Luiz <53343378+luizvruoso@users.noreply.github.com>
2019-10-14 17:53:46 -0300

    try catch
    tratado erros

--- APP Android/app/src/main/java/com/example/control_for_2048/MainActivity.java
+++ APP Android/app/src/main/java/com/example/control_for_2048/MainActivity.java
@@ -49,23 +49,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClickDown(View v) {
-        servidor.initServer();
-        servidor.Write(gerador.CriaObjeto("down").toString());
+        try{
+            servidor.initServer();
+            servidor.Write(gerador.CriaObjeto("down").toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public void onClickUp(View v) {
-        servidor.initServer();
-        servidor.Write(gerador.CriaObjeto("up").toString());
+        try{
+            servidor.initServer();
+            servidor.Write(gerador.CriaObjeto("up").toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void onClickLeft(View v) {
-        servidor.initServer();
-        servidor.Write(gerador.CriaObjeto("left").toString());
+        try{
+            servidor.initServer();
+            servidor.Write(gerador.CriaObjeto("left").toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void onClickRight(View v) {
-        servidor.initServer();
-        servidor.Write(gerador.CriaObjeto("right").toString());
+        try{
+            servidor.initServer();
+            servidor.Write(gerador.CriaObjeto("right").toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
