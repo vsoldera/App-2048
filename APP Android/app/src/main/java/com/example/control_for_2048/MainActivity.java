67c1f7c54f3d83ee0b412ff77e24fe5c4f304cde
VictorSoldera <victorsolderalf@gmail.com>
2019-10-14 17:41:39 -0300

    tensorflow object detection

--- APP Android/app/src/main/java/com/example/control_for_2048/MainActivity.java
+++ APP Android/app/src/main/java/com/example/control_for_2048/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.control_for_2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +16,7 @@ import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button btnTensorFlow;
     final JsonGenerator gerador = new JsonGenerator();
     final Server servidor = new Server("192.168.0.105", 3000); // setar com suas confs de server
 
@@ -23,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTensorFlow = findViewById(R.id.btnTensor);
+        btnTensorFlow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               moveToTensorFlow();
+            }
+            private void moveToTensorFlow() {
+                Intent intent = new Intent(MainActivity.this, DisplayTensorFlow.class);
+                startActivity(intent);
+            }
+        });
         if (android.os.Build.VERSION.SDK_INT > 9) // corretor par funfar na rede local e interwebs
         {
             StrictMode.ThreadPolicy policy = new
@@ -54,4 +67,6 @@ public class MainActivity extends AppCompatActivity {
         servidor.initServer();
         servidor.Write(gerador.CriaObjeto("right").toString());
     }
+
+
 }
