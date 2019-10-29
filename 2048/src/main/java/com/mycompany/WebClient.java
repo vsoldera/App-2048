1d140e89dcf87ab9a52d35eff51d130d153188ca
Luiz <53343378+luizvruoso@users.noreply.github.com>
2019-10-29 10:44:33 -0300

    GET function - fisrt steps

--- 2048/src/main/java/com/mycompany/WebClient.java
+++ 2048/src/main/java/com/mycompany/WebClient.java
@@ -1,47 +1,44 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 public class WebClient {
-     public static void main(String[] args) throws Exception {
-	String url = "http://localhost:443/safe/xd.html";
+    
 
-		URL UrlObj = new URL(url);
 
-		HttpURLConnection connection = (HttpURLConnection) UrlObj.openConnection();
-		connection.setRequestMethod("POST");
-                 System.setProperty("http.agent", "");
+
+	public static void main(String[] args) throws Exception {
+
+		String url = "http://localhost:3000/getPos";
+		URL urlObj = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+
+		connection.setRequestMethod("GET");
 		connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-                
-		connection.setDoOutput(true);
 
-         try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-             String urlPostParameters = "userID=250&amp;userName=Mike";
-             outputStream.writeBytes(urlPostParameters);
-             
-             outputStream.flush();
-         }
+		System.out.println("Send 'HTTP GET' request to : " + url);
 
-		System.out.println("Send 'HTTP POST' request to : " + url);
-		
-		int responseCode = connection.getResponseCode();
+		Integer responseCode = connection.getResponseCode();
 		System.out.println("Response Code : " + responseCode);
 
 		if(responseCode==HttpURLConnection.HTTP_OK) {
-                    StringBuffer response;
-            try (BufferedReader inputReader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-                response = new StringBuffer();
-                while ((inputLine = inputReader.readLine()) != null) {
-                    response.append(inputLine);
-                }
-            }
+			BufferedReader inputReader = new BufferedReader(
+					new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
 
-		System.out.println(response.toString());
-        }
-    }
+			while ((inputLine = inputReader.readLine()) != null) {
+				response.append(inputLine);
+			}
+
+			inputReader.close();
+
+			System.out.println(response.toString());
+		}
+	}
+
 }
+
+
+     
    
